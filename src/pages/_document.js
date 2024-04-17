7e2d2fd7864949369d5d6bde38bbb03e096f6423
ridibrito <devoldbsb@gmail.com>
2024-04-17 17:32:55 -0300

    Pixe

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,67 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Footer from "@/components/footer";
-import Header from "@/components/header";
 
-export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <title>Anadem</title>
-        <link rel="icon" href="/icone.png" />
-      </Head>
-      <body>
-        <Header />
-        <link
-          rel="stylesheet"
-          href="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-button.css"
-        />
-        <a
-          id="robbu-whatsapp-button"
-          target="_blank"
-          href="https://api.whatsapp.com/send?phone=61998615656&text=Ol%C3%A1%20Ricardo,%20gostaria%20de%20um%20diagn%C3%B3stico%20personalizado..."
-        >
-          <div class="rwb-tooltip">Fale comigo agora!</div>
-          <img src="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-icon.svg" />
-        </a>
-        <Main />
-        <NextScript />
-      </body>
-      <Footer />
-    </Html>
-  );
+const FB_PIXEL_ID = "3880645752172054";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br" className="!scroll-smooth">
+        <Head>
+          <title>Anadem - Direito Médico e Bioético</title>
+          <meta
+            name="description"
+            content="Somos uma S.A. (Sociedade Anônima) que promove o debate sobre problemas relacionados ao exercício profissional da medicina e da odontologia. Além disso, oferecemos blindagem jurídica para profissionais da Saúde, por meio de escritórios de advocacia credenciados especializados em Direito Médico e Odontológico."
+          />
+          <meta
+            name="facebook-domain-verification"
+            content="b2fphkly8l3b3v7q70uf1prc3lr2xv"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3917676,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+            }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${FB_PIXEL_ID}');
+                fbq('track', 'pageLoad');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+          <link rel="icon" href="/icone.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <Footer />
+        </body>
+      </Html>
+    );
+  }
 }
