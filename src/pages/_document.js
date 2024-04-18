2a6b4841aa6e5277245b04c36da23b6c5f0f8889
ridibrito <devoldbsb@gmail.com>
2024-04-18 15:24:12 -0300

    tagManager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -57,8 +57,22 @@ export default class MyDocument extends Document {
             />
           </noscript>
           <link rel="icon" href="/icone.png" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TFZ6X542'`,
+            }}
+          />
         </Head>
         <body>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TFZ6X542"
+            height="0"
+            width="0"
+          ></iframe>
           <link
             rel="stylesheet"
             href="https://cdn.positus.global/production/resources/robbu/whatsapp-button/whatsapp-button.css"
